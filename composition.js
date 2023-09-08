fc53a39aa6e8be616fdfd3a3dfd9c2045136e3b9
eggman <eggermann@gmx.ch>
2023-09-08 15:31:23 +0200

    add dailymotion

--- composition.js
+++ composition.js
@@ -34,10 +34,10 @@ const compo = {
     //circularLinksGetNext:()=>{
     //this is the cirular context },
 
-    folderVersionString: 'stamp',// v-{cnt}-{folderVersionString} bear beer
+    folderVersionString: 'dailymotoion',// v-{cnt}-{folderVersionString} bear beer
     //  words: [[':YP', 'en']],['War', 'en'],
-    words: [['anatomy', 'en'], ['war', 'en'], ['life style', 'en']],
-    //  words: [['War', 'en'],['Art', 'en'],['Landscape', 'en']],
+    words: [['ukraine', 'en'],['boobs', 'en']],
+    //  words: [['War', 'en'],['Art', 'en'],['Landscape', 'en']],,['boobs', 'en']
 //    words: [[':YP', 'en']],,['Postage_stamp', 'en']
     //words: [['Robotics', 'en'],[':NewsStream', {startWord: ''}],['Humanities', 'en']],
 
@@ -45,11 +45,11 @@ const compo = {
     //words: [[':NewsStream', {startWord: ''}]],
 
     //  ['Adolescence', 'en']  randomImageOrientations: [' background'],// allStatics,//,['spo-l,m t on ', ' background '],
-    staticPrompt: ' (value), high quality used stamp',//['Style', 'en'],
+   // staticPrompt: ' (value), high quality used stamp',//['Style', 'en'],
     // randomImageOrientations: [' background', ' in the foreground '],
     // randomImageOrientations: [' background', ' spot on', ' together with ', ' act as '],
 //    titts pixel  used stamp
-    model: 'webUi',//'midjourney',//'webUi',//'huggin',// 'webUi',//'midjourney',
+    model: 'dailymotion',//'dailymotion',//'youtube',//'midjourney',//'webUi',//'huggin',// 'webUi',//'midjourney',
     info: ' pinup pixel used stamp',
     "negative_prompt": ' (deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck'
 ,
