1de611045499b768b49971d6ffc5f2ac576d5f18
eggman <eggermann@gmx.ch>
2025-01-21 20:07:19 +0100

    refactor

--- test.composition.js
+++ test.composition.js
@@ -1,4 +1,6 @@
 const words = [['medicine', 'en']];
-const model = 'huggin'
+const scriptName = 'post-to-hugging.js'
 import('./start.js').then(module =>
-    module.default({words, model}));
+    module.default({words, model:{
+            scriptName
+        }}));
