611e8473573b3cd35e2523b8256a395ae1342a36
eggman <eggermann@gmx.ch>
2025-01-21 22:57:25 +0100

    dsf

--- test.composition.js
+++ test.composition.js
@@ -1,4 +1,4 @@
-const words = [['medicine', 'en']];
+const words = [['medicine', 'en'],['wildlife', 'en']];
 const scriptName = 'post-to-hugging.js'
 import('./start.js').then(module =>
     module.default({words, model:{
