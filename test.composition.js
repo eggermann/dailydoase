a7bc140015c792ddf44ce0a7fc356536536b40ed
eggman <eggermann@gmx.ch>
2025-02-22 01:17:18 +0100

    overwork yt

--- test.composition.js
+++ test.composition.js
@@ -1,5 +1,5 @@
-const words = [['medicine', 'en'],['wildlife', 'en']];
-const scriptName = 'post-to-hugging.js'
+const words = [['wildlife', 'en']];
+const scriptName = 'post-to-youtube.js'
 import('./start.js').then(module =>
     module.default({words, model:{
             scriptName
