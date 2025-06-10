3e7cecce1c42b6e14ece72e70deb5a761633ad38
eggman <eggermann@gmx.ch>
2025-06-10 15:16:13 +0200

    add video

--- test.composition.js
+++ test.composition.js
@@ -1,4 +1,4 @@
-const words = [['wildlife', 'en'], ['Robotics', 'en']];//, [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
+const words = [['wildlife', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
 
 /*
 const scriptName = 'post-to-youtube.js'
@@ -10,7 +10,7 @@ import('./start.js').then(module =>
 
 */
 
-
+/*
 const scriptName = 'post-to-genSeq.js'
 import('./start.js').then(module =>
     module.default({
@@ -21,4 +21,22 @@ import('./start.js').then(module =>
             return streams;
         }
     }));
+*/
 
+
+const scriptName = 'post-to-FLUX.js'
+import('./start.js').then(module =>
+    module.default(
+        [
+            {
+                words,
+                model: {
+                    scriptName,
+                    fluxVariant: 'schnell', // or 'dev' for the dev endpoint
+                    // imageDir: path.resolve(__dirname, '../images/flux-test'),
+
+                },
+                folderName: 'FLUX-schnell-compo-test'
+            }
+        ]
+    ));
