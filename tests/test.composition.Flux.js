e42bc4248510392b3a5b7e440c83b6e2e9ecb0be
eggman <eggermann@gmx.ch>
2025-06-27 14:03:45 +0200

    change to FE

--- tests/test.composition.Flux.js
+++ tests/test.composition.Flux.js
@@ -1,34 +1,40 @@
-const words = [['wildlife', 'en'], ['Boobs', 'en'], ['Neon', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
+const words = [['wildlife', 'en'], ['Boobs', 'en'], ['Acid', 'en'], ['Movie', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
 const word2 = [['Davidstern', 'de'], ['Davidstern', 'en'], ['Davidstern', 'fr']];
-
-
 
 
 const scriptName = 'post-to-FLUX.js'
 import('../semantic-stream.js').then(module =>
-module.default(
-    [
-        {
-            words,
-            model: {
-                pollingTime: 20000,
-                scriptName,
-                fluxVariant: 'schnell', // or 'dev' for the dev endpoint
-                // imageDir: path.resolve(__dirname, '../images/flux-test'),
+    module.default(
+        [
+            {
+                staticPrompt: 'as a Donald Trump fan mobile shot',
 
+                words: words,
+                model: {
+                    pollingTime: 15000,
+                    scriptName,
+                    fluxVariant: 'dev', // or 'dev' for the dev endpoint,
+                    guidance_scale:0,
+                    num_inference_steps: 24,
+
+
+
+
+                    // imageDir: path.resolve(__dirname, '../images/flux-test'),
+
+                },
+                folderName: 'XXtest__FLUX-schnell'
             },
-            folderName: 'FLUX-schnell-compo-test'
-        },
 
-    /* {
-              words:word2,
-              pollingTime: 45500,
-              model: {
-                  scriptName,
-                  fluxVariant: 'dev', // schnell or 'dev' for the dev endpoint
- 
-              },
-              folderName: 'FLUX-dev-compo-test'
-          }*/
-    ]
-));
+            /* {
+                      words:word2,
+                      pollingTime: 45500,
+                      model: {
+                          scriptName,
+                          fluxVariant: 'dev', // schnell or 'dev' for the dev endpoint
+         
+                      },
+                      folderName: 'FLUX-dev-compo-test'
+                  }*/
+        ]
+    ));
