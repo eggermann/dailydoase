ab0d76dcfecc54341b889f73234352ab5158180e
eggman <eggermann@gmx.ch>
2025-06-30 12:18:07 +0200

    add chatModelProbe test in post to flux

--- tests/test.composition.Flux.js
+++ tests/test.composition.Flux.js
@@ -1,4 +1,6 @@
-const words = [['wildlife', 'en'], ['Boobs', 'en'], ['Acid', 'en'], ['Movie', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
+
+
+const words = [['Davidstern', 'de'], ['Boobs', 'en'], ['Acid', 'en'], ['Movie', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
 const word2 = [['Davidstern', 'de'], ['Davidstern', 'en'], ['Davidstern', 'fr']];
 
 
@@ -10,8 +12,13 @@ import('../semantic-stream.js').then(module =>
                 staticPrompt: 'as a Donald Trump fan mobile shot',
 
                 words: words,
+                modelProbePrompt:(totalPrompt)=>{
+                    return `create a erotic dirty image description with the following:
+                     ${totalPrompt} <-- use strong adjectives and nouns, no verbs, no adverbs, no articles, no prepositions`;
+                },
+
                 model: {
-                    pollingTime: 15000,
+                    pollingTime: 1000,
                     scriptName,
                     fluxVariant: 'dev', // or 'dev' for the dev endpoint,
                     guidance_scale:0,
