a20e39c955ef97f9a11fea733ccfe260115f24a8
eggman <eggermann@gmx.ch>
2025-07-01 13:13:58 +0200

    rework on Flux format

--- tests/test.composition.IMAGE-VIDEO.js
+++ tests/test.composition.IMAGE-VIDEO.js
@@ -1,38 +1,46 @@
-const scriptName = 'post-to-Img-Video.js'
+const model = 'post-to-Img-Video.js'
 
 import('../semantic-stream.js').then(module =>
     module.default(
         [{
-            staticPrompt: ' as a Donald Trump video in wild western style ',
-            //     words: [['wildlife', 'en']],
-            words: [['Donald Trump', 'en'], ['History', 'en'], ['Pumpkin', 'en']],
-            model: {
-                scriptName
+            model,
+            words: [
+                ['Art', 'en'],
+                ['Aktzeichnen', 'de'],
+                ['Figure_drawing', 'en'],
+                ['Classical_element', 'en']
+            ],
+            staticPrompt: {
+                post: ' as animal '
             },
-            video: {
+            //     words: [['wildlife', 'en']],
 
-                folderName: 'ltxVideos-test',
-                cfg: 3.0,
-                steps: 25,
+            video: {
+                folderName: 'ltxv13bDistilled',
+                cfg: 1.0,
+                steps: 7,
                 motionBucketId: 127,
-                fps: 6,
+                fps: 30,
                 seed: Math.round(1204 * Math.random()),
-              //  imageDir: path.resolve(__dirname, '../images/ltx-test'),
-                height_ui: 512,
-                width_ui: 704,
-                duration_ui: 8,
+                //  imageDir: path.resolve(__dirname, '../images/ltx-test'),
+                height_ui: 1024,
+                width_ui: 576,
+                downscale_factor: 0.6666666,
+                duration_ui: 10,
                 ui_guidance_scale: 1,
+                decode_timestep: 0.05,
+                decode_noise_scale: 0.025,
                 improve_texture_flag: true,
                 negative_prompt: 'worst quality, inconsistent motion, blurry, jittery, distorted',
                 mode: 'image-to-video'
 
             },
             image: {
+                folderName: 'ltxv13bDistilled',
                 fluxVariant: 'schnell'
             },
+
             promptFunktion: async (streams) => {
-
-
                 return streams;
             }
         }]
