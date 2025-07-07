23954d2ff762c0fdc5d80924dfcc3e92ca1bfe9a
eggman <eggermann@gmx.ch>
2025-07-07 20:11:19 +0200

    LEO-and examples

---
+++ tests/test.composition.post-to-create-song-LEO.js
@@ -0,0 +1,56 @@
+
+
+const words = [['Davidstern', 'de'], ['Boobs', 'en'], ['Acid', 'en'], ['Movie', 'en']];//, ['Robotics', 'en'], [':NewsStream', {startWord: ''}], ['Humanities', 'en']];
+const streams = [['Bass', 'en'], ['Sex', 'en'], ['art', 'en'],['Beat', 'en']];
+
+
+
+//const streams = [['lofi', 'en']];//[['lofi', 'en'], ['hip‑hop', 'en'], ['beats', 'en'], ['relax', 'en']];
+
+// Fast-ish options for a CI run
+
+
+
+const scriptName = 'post-to-create-song-LEO.js'
+import('../semantic-stream.js').then(module =>
+    module.default(
+        [
+            {
+
+                words: streams,
+                staticPromptXX: {
+                    pre: 'phone photo of a',
+                    post: ', art performance, contemporary art,art exhibition posted to instagram, raw style'
+                },
+
+                model:
+                    Object.assign(
+                        {
+                            pollingTime: 4000,
+                            scriptName,
+                            //    fluxVariant: 'dev', // or 'dev' for the dev endpoint,
+                            //   guidance_scale: 0,
+                            //   num_inference_steps: 24,
+                            // imageDir: path.resolve(__dirname, '../images/flux-test'),
+                        },
+
+                    ),
+                folderName: 'ace-compo-test',
+                promptFunktion: async (streams) => {
+                    return streams;
+                }
+            },
+
+
+            /* {
+                      words:word2,
+                      pollingTime: 45500,
+                      model: {
+                          scriptName,
+                          fluxVariant: 'dev', // schnell or 'dev' for the dev endpoint
+         
+                      },
+                      folderName: 'FLUX-dev-compo-test'
+                  }*/
+        ]
+    ));
