e7aa7d77dd3b2ba63164237ec00d1a28feb34201
eggman <eggermann@gmx.ch>
2025-07-08 22:23:07 +0200

    veo v_2-951-ginigenVeo3-free

---
+++ tests/test.composition.ginigen-Veo3-free.js
@@ -0,0 +1,36 @@
+const word2 = [['fantasy', 'en'], ['sex', 'en'], ['art', 'en'], ['light performance', 'en']];
+const scriptName = 'post-to-ginigen-Veo3-free.js'
+
+
+
+
+import('../semantic-stream.js').then(module =>
+    module.default(
+        [{
+            model: {
+                scriptName
+            },
+            words: word2,
+            folderName: 'ginigenVeo3-free',
+            staticPrompt: {
+                post: ' as animal '
+            },
+
+            seed: Math.round(1204 * Math.random()),
+            steps: 7,
+            // 4:3 aspect ratio, next higher size, both dimensions multiple of 32 (e.g., 128x168)
+            height: 128, // 128 is a multiple of 32
+            width: 168,  // 168 is a multiple of 32, 168/128 = 4/3
+            duration_seconds: 7,
+            nag_scale: 12,
+            audio_steps:30,
+            audio_cfg_strength: 0.5,
+
+            promptFunktion: async (streams) => {
+                return streams;
+            }
+        }]
+    )).catch(err => {
+        console.error('Error in start.js:', err);
+        process.exit(1);
+    });
