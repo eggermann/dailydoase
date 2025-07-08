870fd383ebb66f2b5eba08026d8d748b61495d95
eggman <eggermann@gmx.ch>
2025-07-08 22:48:41 +0200

    add baseprompt

--- tests/test.composition.ginigen-Veo3-free.js
+++ tests/test.composition.ginigen-Veo3-free.js
@@ -13,18 +13,19 @@ import('../semantic-stream.js').then(module =>
             words: word2,
             folderName: 'ginigenVeo3-free',
             staticPrompt: {
-                post: ' as animal '
+              //  pre: 'a handycam video of a',
+                post: ' as animals '
             },
 
             seed: Math.round(1204 * Math.random()),
-            steps: 7,
+            steps: 8,//8max
             // 4:3 aspect ratio, next higher size, both dimensions multiple of 32 (e.g., 128x168)
-            height: 128, // 128 is a multiple of 32
-            width: 168,  // 168 is a multiple of 32, 168/128 = 4/3
+            height: 128 , // 128 is a multiple of 32
+            width: 168 ,  // 168 is a multiple of 32, 168/128 = 4/3
             duration_seconds: 7,
             nag_scale: 12,
-            audio_steps:30,
-            audio_cfg_strength: 0.5,
+            audio_steps: 50,
+            audio_cfg_strength: 0.8,
 
             promptFunktion: async (streams) => {
                 return streams;
