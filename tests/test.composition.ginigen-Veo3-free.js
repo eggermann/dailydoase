4fc2126b3ac87315e1d43ac4ca57488a311b2583
eggman <eggermann@gmx.ch>
2025-07-11 13:47:01 +0200

    fix img

--- tests/test.composition.ginigen-Veo3-free.js
+++ tests/test.composition.ginigen-Veo3-free.js
@@ -1,27 +1,30 @@
-const word2 = [['Worm', 'en'], ['sex', 'en'], ['art', 'en'], ['Ratte', 'de']];
+const word2 = [['art', 'en'], ['human body', 'en'], ['history', 'en']];
 const scriptName = 'post-to-ginigen-Veo3-free.js'
-
-
 
 
 import('../semantic-stream.js').then(module =>
     module.default(
         [{
             model: {
-                scriptName
+                scriptName,
+                "link": {
+                    "url": "ginigen/VEO3-Free",
+                    "name": "ginigen VEO3-Free",
+                    "alt": "the model name on hugginface"
+                }
             },
             words: word2,
             folderName: 'ginigenVeo3-free',
             staticPrompt: {
-              //  pre: 'a handycam video of a',
-                post: ' as horror movie, raw style'
+                 pre: 'the fallowing is a video of a social media post :',
+                post: ' raw style, eerie atmosphere',
             },
 
             seed: Math.round(1204 * Math.random()),
             steps: 4,//8max
             // 4:3 aspect ratio, next higher size, both dimensions multiple of 32 (e.g., 128x168)
-            height: 128 , // 128 is a multiple of 32
-            width: 168 ,  // 168 is a multiple of 32, 168/128 = 4/3
+            height: 160, // 128 is a multiple of 32
+            width: 190,  // 168 is a multiple of 32, 168/128 = 4/3
             duration_seconds: 8,
             nag_scale: 11,
             audio_steps: 50,
